576ea4764586eea159f5c72b55b11c9e3cd5cc2f
shaarkys <sharkys@email.cz>
2024-06-09 12:58:48 +0200

    Icons cosmetic sRM3 / RM4 Mini / Pro + fixed settings error for first time instalations

--- settings/js/settings.js
+++ settings/js/settings.js
@@ -1,47 +1,54 @@
-function saveDebugSettings (Homey) {
+function saveDebugSettings(Homey) {
+  var compatmode = document.getElementById('compatmode') ? document.getElementById('compatmode').checked : false;
+  var debuglog = document.getElementById('debuglog') ? document.getElementById('debuglog').checked : false;
+  // var errorreport = false;
+  // try {
+  //    errorreport = document.getElementById('errorreport').checked;
+  // }
+  // catch( err ){;}
+
+  var currentSettings = {
+    'compat': compatmode,
+    'logging': debuglog,
+    // 'errorreport': errorreport
+  }
+  Homey.set('DebugSettings', currentSettings);
+}
+
+function onHomeyReady(Homey) {
+  var compatmodeElement = document.getElementById('compatmode');
+  var debuglogElement = document.getElementById('debuglog');
+  // var errorreportElement = document.getElementById('errorreport');
+
+  if (compatmodeElement) {
+    compatmodeElement.addEventListener('change', function(e) { saveDebugSettings(Homey); })
+  }
+  if (debuglogElement) {
+    debuglogElement.addEventListener('change', function(e) { saveDebugSettings(Homey); })
+  }
   
-   var compatmode  = document.getElementById('compatmode').checked;
-   var debuglog    = document.getElementById('debuglog').checked;
-   // var errorreport = false;
-   // try {
-   //    errorreport = document.getElementById('errorreport').checked;
-   // }
-   // catch( err ){;}
+  // if (errorreportElement) {
+  //    errorreportElement.addEventListener('change', function(e) { saveDebugSettings(Homey); })
+  // }
    
-   var currentSettings = {
-     'compat'      : compatmode,
-     'logging'     : debuglog,
-     // 'errorreport' : errorreport
-   }
-   Homey.set('DebugSettings', currentSettings );
- }
- 
- 
- function onHomeyReady( Homey ) {
- 
-   var compatmodeElement  = document.getElementById('compatmode');
-   var debuglogElement    = document.getElementById('debuglog');
-   // var errorreportElement = document.getElementById('errorreport');
- 
-   compatmodeElement.addEventListener( 'change', function( e ) { saveDebugSettings(Homey); })
-   debuglogElement.addEventListener(   'change', function( e ) { saveDebugSettings(Homey); })
-   
-   // if( errorreportElement ) {
-   //    errorreportElement.addEventListener('change', function( e ) { saveDebugSettings(Homey); })
-   // }
-    
-   Homey.get('DebugSettings', function (error, currentSettings) {
-     if (error) {
-       Homey.ready();
-       Homey.alert(error,"error",null);
-     }
-     compatmodeElement.checked  = currentSettings.compat;
-     debuglogElement.checked    = currentSettings.logging;
-       
-     // if( errorreportElement ) { errorreportElement.checked = currentSettings.errorreport; }
- 
-     Homey.ready();
-   });
-    
-   Homey.ready();
- } 
+  Homey.get('DebugSettings', function(error, currentSettings) {
+    if (error || !currentSettings) {
+      Homey.ready();
+      if (error) {
+        Homey.alert(error, "error", null);
+      }
+      return;
+    }
+    if (compatmodeElement) {
+      compatmodeElement.checked = currentSettings.compat || false;
+    }
+    if (debuglogElement) {
+      debuglogElement.checked = currentSettings.logging || false;
+    }
+    // if (errorreportElement) { errorreportElement.checked = currentSettings.errorreport || false; }
+
+    Homey.ready();
+  });
+  
+  Homey.ready();
+}
